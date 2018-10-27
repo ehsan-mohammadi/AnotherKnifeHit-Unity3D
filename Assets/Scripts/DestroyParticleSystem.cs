1229a2ec1bd7e2b11bf2b39c5d5667b78fca7c23
ehsan-mohammadi <mohammadi.ehsan1994@gmail.com>
2018-10-27 22:44:37 +0330

    Assets updated

---
+++ Assets/Scripts/DestroyParticleSystem.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyParticleSystem : MonoBehaviour {
+
+    public float lifeTime = 0f;
+
+	// Use this for initialization
+	void Start () 
+    {
+        // Destroy after a time
+        Destroy(gameObject, lifeTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
